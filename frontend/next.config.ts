855ded6fa8fd0ec8ec645961050e295def6c5b9b
Tanisha <tanisha.nahak.1803@gmail.com>
2026-09-13 11:37:21 +0530

    fix: add vercel.json configurations to resolve 404 deployment error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-import path from "path";
-
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname),
+  reactStrictMode: true,
 };
 
 export default nextConfig;
